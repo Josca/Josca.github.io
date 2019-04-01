b3c3bc26761627cc9b5f64f998c4634e6f56afb3
hakjosef <josef.hak@firma.seznam.cz>
2019-04-02 00:03:42 +0200

    added augumented image configurable

--- augumented_image/script.js
+++ augumented_image/script.js
@@ -1,21 +1,33 @@
-imgname = "chb.jpg"
+var paramsEncoded = window.location.search.substr(1);  // get query params as one string
+var paramsDecoded = decodeURIComponent(paramsEncoded); // url-decode it
+var paramsList = paramsDecoded.split("&");             // parse particular params
 
+// detect if edit mode is on using 3rd param "edit"
+var allowEdit = paramsList.length == 3 && paramsList[2] == "edit";
+
+var imageUrl = paramsList[0]; // first param is input image url
 var img = new Image;
-img.src = imgname;
+img.src = imageUrl;
 var labelOffset = 15;
 
-c = document.getElementById('container');
-c.style.backgroundImage = "url('" + imgname +   "')";
+containerDiv = document.getElementById('container');
+containerDiv.style.backgroundImage = "url('" + imageUrl +   "')";
+
+var textEdit = document.getElementById('textEdit');
+if (!allowEdit) {
+  textEdit.style.display = "none";
+}
 
 var svgns = "http://www.w3.org/2000/svg";
 var svgElem = document.getElementById('svg');
 var labelTextElem = document.getElementById('labelText');
 var labelRectElem = document.getElementById('labelRect');
 
-var items = [
-  {"x": 473, "y": 880, "desc": "Chrám sv. Víta, Václava a Vojtěcha"},
-  {"x": 2838, "y": 1012, "desc": "Socha sv. Jana Nepomuckého"}
-];
+var itemsDecoded = paramsList[1];
+var items = []
+if (itemsDecoded) {
+  items = JSON.parse(itemsDecoded);
+}
 
 window.onload = function () {
   // get scale quotient
@@ -24,12 +36,10 @@ window.onload = function () {
   var fScale = Math.min(fw, fh)
 
   // scale svg element with scale quotient
-  svgElem.setAttribute("width", fScale * img.width)
-  svgElem.setAttribute("height", fScale * img.height)
+  svgElem.setAttribute("width", fScale * img.width);
+  svgElem.setAttribute("height", fScale * img.height);
 
-  console.log("fw:", fw, "fh:", fh, "fScale:", fScale);
-
-  items.forEach(function (item, i) {
+  function drawPoint(item) {
     var circle = document.createElementNS(svgns, 'circle');
     circle.setAttributeNS(null, 'cx', item.x * fScale);
     circle.setAttributeNS(null, 'cy', item.y * fScale);
@@ -53,5 +63,24 @@ window.onload = function () {
     });
 
     svgElem.appendChild(circle);
+  }
+
+  // draw svg circles - annotated points
+  items.forEach(function (item, i) {
+    drawPoint(item);
   });
+
+  // enable points adding using mouse click on image
+  if (allowEdit) {
+    containerDiv.addEventListener('mousedown', function(e) {
+      var x = Math.round(e.clientX / fScale);
+      var y = Math.round(e.clientY / fScale);
+      var m = {"x": x, "y": y, "desc": textEdit.value};
+      drawPoint(m);
+      textEdit.value = "";
+
+      items.push(m);
+      alert(JSON.stringify(items));
+    });
+  }
 }
